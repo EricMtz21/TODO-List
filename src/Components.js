2f8be4b0da63bab700b707be5573e9f7265cfa7c
Eric <ericmage2001@gmail.com>
2023-10-15 22:36:11 -0600

    First commit, LogIn container made

---
+++ src/Components.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function myButton () {
+
+}
+
+export function View () {
+    return (
+        <div className="container">
+            <br></br>
+            <h1>Log In</h1>
+        </div>
+    )
+}
